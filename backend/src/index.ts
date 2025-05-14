41a22f19bbe27f64716b5d7c98c8821fd0cf7954
DoanDatt <doancongdat2004@gmail.com>
2025-05-14 13:18:47 +0700

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import express, { Response, Request } from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/test", (req: Request, res: Response) => {
+  res.json({
+    message: "Hello from the backend!",
+  });
+});
+
+app.listen(7000, () => {
+  console.log("Server is running on port 7000");
+});
