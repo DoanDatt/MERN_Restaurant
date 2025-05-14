136a1b494606879d5bb92bb0ef35006d8226a216
DoanDatt <doancongdat2004@gmail.com>
2025-05-14 13:47:22 +0700

    connected Database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,15 @@
 import express, { Response, Request } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
+  console.log("Connected to MongoDB");
+});
 app.get("/test", (req: Request, res: Response) => {
   res.json({
     message: "Hello from the backend!",
