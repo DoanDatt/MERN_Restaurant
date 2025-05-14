2647c69d0cea3f3e81aee65b8fd7a194ba91a2e1
DoanDatt <doancongdat2004@gmail.com>
2025-05-14 13:28:51 +0700

    frontend-setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button className="bg-red-400">Click me</Button>
+    </div>
+  );
+}
+
+export default App;
