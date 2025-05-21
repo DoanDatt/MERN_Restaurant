504b14f313e3c368b0dd7ff53cd8bdb6b85acc29
DoanDatt <doancongdat2004@gmail.com>
2025-05-21 09:57:24 +0700

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <Button className="bg-button hover:bg-hoverButton">Click me</Button>
+    </div>
+  );
+}
+
+export default App;
